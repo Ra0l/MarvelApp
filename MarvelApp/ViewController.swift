b33d646823466ded6d7033324439efc4fc212e3f
Raul Kevin Aliaga Shapiama <45242019+Ra0l@users.noreply.github.com>
2023-04-24 10:45:17 -0500

    Start project_2

---
+++ MarvelApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MarvelApp
+//
+//  Created by Raul Kevin Aliaga Shapiama on 4/20/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
